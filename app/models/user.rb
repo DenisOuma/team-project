3bc39f465c08dae12ad706e4a06b65c084a8aa73
DenisOuma <dennisouma38@gmail.com>
2022-12-04 01:48:21 +0300

    generating Usrs models and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ApplicationRecord
-    # password=, password_confirmation, authenticate
-    has_secure_password 
 end
---
+++ test/controllers/add_password_digest_to_users_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AddPasswordDigestToUsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
