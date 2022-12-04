b62ecbfa8f601a945e5a5b6e4dac3cd7c9e6383a
DenisOuma <dennisouma38@gmail.com>
2022-12-04 03:22:35 +0300

    Additional routes to add users, display user by Id and list of all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
+    # GET /users    ==> Get all users from the db
+    def index
+        users =User.all
+        render json: users
+    end
 
-def create 
-    user = User.create!(user_params)
-    render json: user, status: :created
-    # byebug
+    # GET /users/:id   ==> Display userd  by Id
+    def show
+        user = user_by_id
+        render json: user
+    end
 
-end
-
+    # POST /users    ==> Add userd to db or Create User
+    def create 
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
 
     private
 
+    # Find User By Id Method
+    def user_by_id
+        user = User.find(params[:id])
+    end
+
+    # Add User Parameters
     def user_params
         params.permit(:username, :email, :password, :password_confirmation)
     end
